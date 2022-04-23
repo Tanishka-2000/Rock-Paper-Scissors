99f6181b7af70d947bb21471747d5a2e57e9d5e6
Tanishka Talwar <tanishkatalwar18@gmail.com>
2022-04-23 12:56:07 +0530

    write algorithm for the project

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
+
+// select a random choice for computer
+// capture the selection of the user
+// validate the selection of the user
+// make varibles to store winning combinations and choices
+
+// pass the choices to the function playRound
+// check for wins or ties
+// increment the score of the winnng player
+// return a statement to be logged to the console
+
+// repeat above procedure 5 times
+
+
+
 function playRound(playerSelection, computerSelection) {
   // your code here!
 }
