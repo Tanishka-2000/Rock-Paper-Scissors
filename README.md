7fc577f478f686e4a7a9a3f1e0c69b26365b7357
Tanishka Talwar <tanishkatalwar18@gmail.com>
2023-03-14 16:18:25 +0530

    show computer's choice on screen

---
+++ README.md
@@ -0,0 +1,8 @@
+# Rock Paper Scissors
+Our childhood game that has settled so many disputes for so long.
+
+## Table of Contents
++ 
+
+## General Info
+This is a virtual form of the traditional 'Rock Paper Scissors' game but you will be playing against computer this time.
--- script.js
+++ script.js
@@ -1,33 +1,33 @@
 // make varibles to store winning combinations and choices
-    const choices = ["rock", "paper", "scissors"];
-    const winningCombinations = {
-      rock: "paper",
-      paper: "scissors",
-      scissors: "rock"
-    };
-    let computerScore = 0;
-    let playerScore = 0;
+const choices = ["rock", "paper", "scissors"];
+const winningCombinations = {
+  rock: "paper",
+  paper: "scissors",
+  scissors: "rock"
+};
+let computerScore = 0;
+let playerScore = 0;
 
-    /////////// DOM MANIPULATION //////////////////
+/////////// DOM MANIPULATION //////////////////
 
-    let buttons = document.querySelectorAll(".game");
-    let resultDiv = document.querySelector(".result-container");
-    let paras = resultDiv.querySelectorAll("p");
-    let coverButton = document.querySelector(".cover button");
+let buttons = document.querySelectorAll(".game");
+let resultDiv = document.querySelector(".result-container");
+let paras = resultDiv.querySelectorAll("p");
+let coverButton = document.querySelector(".cover button");
 
-    coverButton.addEventListener("click", function(e){
-        e.target.parentElement.style.display = "none";
-    });
+coverButton.addEventListener("click", function(e){
+    e.target.parentElement.style.display = "none";
+});
 
-    buttons.forEach(button => {
-        button.addEventListener("click",playGame);
-    });
+buttons.forEach(button => {
+    button.addEventListener("click",playGame);
+});
 
 // select a random choice for computer
-    function getComputerChoice(){
-        let num = Math.floor(Math.random()*3);
-        return choices[num];
-    }
+function getComputerChoice(){
+    let num = Math.floor(Math.random()*3);
+    return choices[num];
+}
 
 //  NOT REQUIRED ANYMORE AS INPUT CAPTURED AS BUTTON CLICK NOT TEXT
 // // capture the selection of the user
@@ -43,46 +43,47 @@
 //     }
 
 // pass the choices to the function playRound
-    function playRound(playerSelection, computerSelection) {
-      let result = "";
-      // check for wins or ties
-      if(playerSelection === computerSelection){
-        result = `It's a tie`;
-      }else if(computerSelection === winningCombinations[playerSelection]){
-        result = `You Lose! ${computerSelection} beats ${playerSelection}`;
-        // increment the score of the winnng player
-        computerScore++;
-      }else{
-        result = `You Win! ${playerSelection} beats ${computerSelection}`;
-        // increment the score of the winnng player
-        playerScore++;
-      }
-      // return a statement to be logged to the console
-      return result;
-    }
+function playRound(playerSelection, computerSelection) {
+  let result = "";
+  // check for wins or ties
+  if(playerSelection === computerSelection){
+    result = `It's a tie`;
+  }else if(computerSelection === winningCombinations[playerSelection]){
+    result = `You Lose! ${computerSelection} beats ${playerSelection}`;
+    // increment the score of the winnng player
+    computerScore++;
+  }else{
+    result = `You Win! ${playerSelection} beats ${computerSelection}`;
+    // increment the score of the winnng player
+    playerScore++;
+  }
+  // return a statement to be logged to the console
+  return result;
+}
 
 // play game whenever the user clicks on any of the button
-    function playGame(e){
-        //playing the game
-            let playerSelection = e.target.id;
-            let computerSelection = getComputerChoice();
-            let result = playRound(playerSelection, computerSelection)
+function playGame(e){
+  //playing the game
+  let playerSelection = e.target.id;
+  let computerSelection = getComputerChoice();
+  let result = playRound(playerSelection, computerSelection)
 
-            // showing the result
-            paras[0].textContent = `Your Score: ${playerScore}`;
-            paras[1].textContent = `Computer Score: ${computerScore}`;
-            paras[2].textContent = `${result}`;
+  // showing the result
+  paras[0].textContent = `Your Choice: ${playerSelection}`;
+  paras[1].textContent = `Computer Choice: ${computerSelection}`;
+  paras[2].textContent = `${result}`;
+  paras[3].textContent = `Your Score: ${playerScore}`;
+  paras[4].textContent = `Computer Score: ${computerScore}`;
 
-            // checking for final win i.e first to score 5 points
-            if(playerScore === 5 || computerScore ===5){
-                let finalResult = " Match over: ";
-                finalResult += playerScore > computerScore ? "You Win!": "You Lose!";
-                playerScore=0;
-                computerScore=0;
-                
-                //showing the result
-                coverButton.previousElementSibling.textContent = `${finalResult}`;
-                coverButton.parentElement.style.display = "block";
-            }
-
-    }
+  // checking for final win i.e first to score 5 points
+  if(playerScore === 5 || computerScore ===5){
+    let finalResult = " Match over: ";
+    finalResult += playerScore > computerScore ? "You Win!": "You Lose!";
+    playerScore=0;
+    computerScore=0;
+    
+    //showing the result
+    coverButton.previousElementSibling.textContent = `${finalResult}`;
+    coverButton.parentElement.style.display = "block";
+  }
+}
